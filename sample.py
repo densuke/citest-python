903323a246d3ab5c9151ddfbb4a7096e45a9c9c2
SATO Daisuke <densuke@fuga.jp>
2019-05-24 05:34:00 +0900

    initial import

---
+++ sample.py
@@ -0,0 +1,17 @@
+class hw:
+    def hello_world(self, msg="world"):
+        '''
+        いわゆるHelloWorld
+        >>> d = hw()
+        >>> d.hello_world()
+        'Hello, world'
+        >>> d = hw()
+        >>> d.hello_world("hoge")
+        'Hello, hoge'
+        '''
+        return "Hello, "
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    
